6d81fccff825331878a3f2833e9afa8220e4b8db
alex.buratta <fulminealex03@gmail.com>
2020-10-23 21:19:52 +0200

    Aggiungere i file di progetto.

---
+++ Lampadine/Lampadina.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Lampadine
+{
+    public class Lampadina
+    {
+
+        public const int STATO_ACCESO = 1;
+        public const int STATO_SPENTO = 0;
+        public const int STATO_ROTTA = -1;
+        public const int STATO_PRIMA_DELLA_ROTTURA = 9;
+        private int stato;
+        private string colore;
+
+        public Lampadina()
+        {
+            this.stato = stato;
+            this.colore = colore;
+        }
+        public int GetClick()
+        {
+
+        }
+        public string GetDecrizione()
+        {
+
+        }
+    }
+}
---
+++ Test_Lampadina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Lampadina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
